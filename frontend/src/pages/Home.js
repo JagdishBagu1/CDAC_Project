3960e8934a269332ee79881594e94b8efd5f8028
Ashwani <ak341801711@gmail.com>
2023-03-08 22:31:52 +0530

    Added Header image in Home page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,4 +1,4 @@
-import { CssBaseline } from '@mui/material';
+import { Box, Container, CssBaseline, Typography } from '@mui/material';
 import React from 'react';
 import NavBar from '../components/NavBar';
 
@@ -7,6 +7,37 @@ export default function Home() {
     <>
       <CssBaseline />
       <NavBar />
+      <Box component="main" sx={{
+        minHeight: '100vh'
+      }}>
+        <Container maxWidth="xl">
+          <Box sx={{
+            my: 2,
+            position: 'relative',
+            minWidth: '100%',
+            height: '250px',
+            backgroundImage: 'url(./images/4.jpg)',
+            color: '#ffffff',
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'center',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}>
+            <Typography variant='h3' sx={{
+              width: '50%',
+              textAlign: 'center',
+              textShadow: '2px 2px 10px black'
+            }}>We are working to become one of the best.</Typography>
+          </Box>
+          <Box sx={{
+            borderTop: '2px solid #cccccc'
+          }}>
+            Blog list here
+          </Box>
+        </Container>
+      </Box>
     </>
   );
 }

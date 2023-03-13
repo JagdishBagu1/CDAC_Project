cf453af3c19f4d092685e18e1065479515941a7a
Ashwani <ak341801711@gmail.com>
2023-03-13 09:31:22 +0530

    jwt authentication completed but not able to allow access to /api/auth/login endpoint access. needed to be fixed

--- backend/CDAC_Blogging_App/src/main/java/com/cdac/security/JWTUtils.java
+++ backend/CDAC_Blogging_App/src/main/java/com/cdac/security/JWTUtils.java
@@ -1,38 +1,58 @@
 package com.cdac.security;
 
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.JWTVerifier;
-import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.exceptions.JWTCreationException;
-import com.auth0.jwt.exceptions.JWTVerificationException;
-import com.auth0.jwt.interfaces.DecodedJWT;
+import com.cdac.constants.Constants;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
 
 @Component
 public class JWTUtils {
 
-    private String secret = "TOP secret";
+    private final String SECRET = "TOP secret";
 
-    public String generateToken(String email) throws IllegalArgumentException, JWTCreationException {
-        return JWT
-                .create()
-                .withSubject("User Details")
-                .withClaim("email", email)
-                .withIssuedAt(new Date())
-                .withIssuer("com/cdac/blog")
-                .sign(Algorithm.HMAC256(secret));
+    public String getUsernameFromToken(String token) {
+        return getClaimFromTOken(token, Claims::getSubject);
     }
 
-    public String validateTokenAndRetrieveSubject(String token) throws JWTVerificationException {
-        JWTVerifier verifier = JWT
-                .require(Algorithm.HMAC256(secret))
-                .withSubject("User Details")
-                .withIssuer("com/cdac/blog")
-                .build();
-        DecodedJWT jwt = verifier.verify(token);
-        return jwt.getClaim("email").asString();
+    public Date getExpirationDateFromToken(String token) {
+        return getClaimFromTOken(token, Claims::getExpiration);
+    }
+
+    public <T> T getClaimFromTOken(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = getAllClaimsFromToken(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims getAllClaimsFromToken(String token) {
+        return Jwts.parser().setSigningKey(SECRET).parseClaimsJws(token).getBody();
+    }
+
+    private Boolean isTokenExpired(String token) {
+        return getExpirationDateFromToken(token).before(new Date());
+    }
+
+    public String generateToken(UserDetails userDetails) {
+        Map<String, Object> claims = new HashMap<>();
+        return doGenerateToken(claims, userDetails.getUsername());
+    }
+
+    private String doGenerateToken(Map<String, Object> claims, String subject) {
+
+        return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + Constants.JWT_TOKEN_VALIDITY * 1000))
+                .signWith(SignatureAlgorithm.HS512, SECRET).compact();
+    }
+
+    public Boolean validateToken(String token, UserDetails userDetails) {
+        final String username = getUsernameFromToken(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
     }
 
 }
---
+++ backend/CDAC_Blogging_App/src/main/java/com/cdac/security/JwtAuthenticationFilter.java
@@ -0,0 +1,78 @@
+package com.cdac.security;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+    @Autowired
+    private JWTUtils jwtUtils;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        // extract Authorization header from request
+        final String authorizationHeader = request.getHeader("Authorization");
+        System.out.println(authorizationHeader);
+
+        String username = null;
+        String token = null;
+
+        // check for correct format
+        if (request != null && authorizationHeader.startsWith("Bearer ")) {
+            // extract token form authorization header
+            token = authorizationHeader.substring(7);
+
+            try {
+                // extract username from token
+                username = this.jwtUtils.getUsernameFromToken(token);
+            } catch (IllegalArgumentException e) {
+                System.out.println("Unable to get jwt token.");
+            } catch (ExpiredJwtException e) {
+                System.out.println("Token has expired.");
+            } catch (MalformedJwtException e) {
+                System.out.println("Invalid jwt.");
+            }
+
+        } else {
+            System.out.println("Token format invalid, not starting with Bearer .");
+        }
+
+        // Now Validate user and authenticate
+        if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
+
+            if (jwtUtils.validateToken(token, userDetails)) {
+                UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+            } else {
+                System.out.println("Invalid token.");
+            }
+
+        } else {
+            System.out.println("Username is null or Context is not null.");
+        }
+
+        filterChain.doFilter(request, response);
+
+    }
+}

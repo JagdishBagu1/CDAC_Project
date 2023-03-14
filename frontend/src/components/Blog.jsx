63417689aaa275c2e85bcae501d7e7549ebef53d
Ashwani <ak341801711@gmail.com>
2023-03-14 23:07:44 +0530

    Home page ui complete, fine looking

--- frontend/src/components/Blog.jsx
+++ frontend/src/components/Blog.jsx
@@ -13,13 +13,21 @@ import { Link } from "react-router-dom";
 
 function Blog({ data }) {
   return (
-    <Card sx={{ my: 2 }}>
+    <Card sx={ { my: 2 } }>
       <Grid container>
-        <Grid item xs={12} sm={8} lg={9} md={9}>
+        <Grid item xs={ 12 } sm={ 12 } md={ 4 } lg={ 3 }>
+          <CardMedia
+            component="img"
+            height="170"
+            image={ `${process.env.REACT_APP_IMAGE_SERVER_URL}/${data.imageUrl}` }
+            alt="green iguana"
+          />
+        </Grid>
+        <Grid item xs={ 12 } sm={ 8 } md={ 8 } lg={ 9 }>
           <CardContent>
-            <Typography variant="caption" component={"span"} sx={{ my: 2 }}>
+            <Typography variant="caption" component={ "span" } sx={ { my: 2 } }>
               <Avatar
-                sx={{
+                sx={ {
                   bgcolor: deepOrange[500],
                   width: 25,
                   height: 25,
@@ -27,46 +35,39 @@ function Blog({ data }) {
                   display: "inline-grid",
                   mr: 1,
                   textTransform: 'uppercase'
-                }}
+                } }
               >
-                {data.user.firstName[0]}{data.user.lastName[0]}
+                { data.user.firstName[0] }{ data.user.lastName[0] }
               </Avatar>
-              {data.user.firstName.toUpperCase()} {data.user.lastName.toUpperCase()}
+              { data.user.firstName.toUpperCase() } { data.user.lastName.toUpperCase() }
             </Typography>
             <br />
-            <Typography gutterBottom variant="h6" sx={{ textTransform: 'capitalize', textDecoration: 'none' }} component={Link} to={`/blogDetails/${data.id}`} >
-              {data.title}
+            <Typography gutterBottom variant="h6" sx={ { textTransform: 'capitalize', textDecoration: 'none' } } component={ Link } to={ `/blogDetails/${data.id}` } >
+              { data.title }
             </Typography>
             <Typography variant="body2" color="text.secondary">
-              {data.content.substring(0, 200) + "..."}
+              { data.content.substring(0, 200) + "..." }
             </Typography>
             <Typography
               variant="body2"
-              display="inline"
-              sx={{ fontWeight: 600, color: "#818181" }}
+              display="inline-block"
+              sx={ { fontWeight: 600, color: "#818181" } }
             >
-              {/* {data.updatedAt} */}
-              {dateFormat(data.updatedAt, "mmm d, yyyy")}
+              {/* {data.updatedAt} */ }
+              { dateFormat(data.updatedAt, "mmm d, yyyy, hh:ss") }
             </Typography>
-            <Typography display="inline">
+            <Typography display="inline" variant="div">
               <Chip
                 size="sm"
                 variant="soft"
-                sx={{ px: 1.5, fontWeight: 700, ml: 5 }}
+                sx={ { px: 1.5, fontWeight: 700, ml: 1 } }
               >
-                {data.category.name}
+                { data.category.name }
               </Chip>
             </Typography>
           </CardContent>
         </Grid>
-        <Grid item xs={12} sm={4} lg={3} md={3}>
-          <CardMedia
-            component="img"
-            height="170"
-            image={`${process.env.REACT_APP_IMAGE_SERVER_URL}/${data.imageUrl}`}
-            alt="green iguana"
-          />
-        </Grid>
+
       </Grid>
     </Card>
   );
